7dcc4f96f4fcdc21192bf2c37e4f7e837dc9d212
Kleight <martinwongso234@gmail.com>
2021-01-09 22:02:44 +0700

    Revert "Merge branch 'main' of https://github.com/jerichosiahaya/rsa-web into main"
    This reverts commit 1e0f1b731f532dd2823b1a057ac3420afb496496, reversing
    changes made to 7439ac89160ce6a0bf529605ecb16bb0823c2e8e.

---
+++ insert_proses.php
@@ -0,0 +1,20 @@
+<?php
+include_once("config.php");
+$nama = $_POST['nama'];
+$alamat = $_POST['alamat'];
+$telepon = $_POST['telepon'];
+$noRangka = $_POST['noRangka'];
+$noPolisi = $_POST['noPolisi'];
+$tglBeli = $_POST['tglBeli'];
+$tglServisTerakhir = $_POST['tglServisTerakhir'];
+$tglServisSelanjutnya = $_POST['tglServisSelanjutnya'];
+$sql1 = "insert into pelanggan (nama, alamat, telepon) values ('$nama', '$alamat', '$telepon')";
+$sql2 = "insert into mobil (noRangka, noPolisi, tglBeli) values ('$noRangka', '$noPolisi', '$tglBeli')";
+$sql3 = "insert into detail_servis (tglServisTerakhir, tglServisSelanjutnya, noRangka) values ('$tglServisTerakhir', '$tglServisSelanjutnya', '$noRangka')";
+//update pelanggan set nama = 'Jericho Sia', alamat = 'Jln. Yabansai No 2 Perumnas 1 Waena', telepon = 82238463057 where id = 2
+if (mysqli_query($conn, $sql1) && mysqli_query($conn, $sql2) && mysqli_query($conn, $sql3)) {
+    echo json_encode(array("statusCode" => 200));
+} else {
+    echo json_encode(array("statusCode" => 201));
+}
+mysqli_close($conn);
